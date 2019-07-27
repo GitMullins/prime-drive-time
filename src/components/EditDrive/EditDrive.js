35907438a30f20f13603323d53c756147734a1b2
David Mullins <Mullins6323@gmail.com>
2019-07-27 18:35:16 -0500

    edit and delete buttons also working on easy-access FiveDayView

--- src/components/EditDrive/EditDrive.js
+++ src/components/EditDrive/EditDrive.js
@@ -3,20 +3,24 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import tripsData from '../../helpers/data/tripsData';
+import routesData from '../../helpers/data/routesData';
 
 const defaultTrip = {
   date: '',
   startTime: '',
   endTime: '',
+  routeId: '',
 };
 
 class EditDrive extends React.Component {
   state = {
     newTrip: defaultTrip,
+    routes: [],
   }
 
   componentDidMount() {
     const tripId = this.props.match.params.id;
+    this.getRoutes();
     tripsData.getSingleTrip(tripId)
       .then(tripPromise => this.setState({ newTrip: tripPromise.data }))
       .catch(err => console.error('could not find drive', err));
@@ -28,11 +32,29 @@ class EditDrive extends React.Component {
     this.setState({ newTrip: tempTrip });
   }
 
+  getRoutes = () => {
+    const { uid } = firebase.auth().currentUser;
+    routesData.getMyRoutes(uid)
+      .then(routes => this.setState({ routes }))
+      .catch(err => console.error(err, 'could not get routes from Home'));
+  }
+
   dateChange = e => this.formFieldStringState('date', e);
 
   startTimeChange = e => this.formFieldStringState('startTime', e);
 
   endTimeChange = e => this.formFieldStringState('endTime', e);
+
+  routeChange = e => this.formFieldStringState('routeId', e);
+
+  routesToDropdown = () => {
+    const { routes } = this.state;
+    const values = [<option key={'chooseRoute'} value={'choose route'} defaultValue>CHOOSE ROUTE</option>];
+    routes.forEach((route) => {
+      values.push(<option value={route.id} key={route.origin}>{route.origin}</option>);
+    });
+    return values;
+  }
 
   onSubmit = (e) => {
     e.preventDefault();
@@ -50,10 +72,13 @@ class EditDrive extends React.Component {
       <div className="EditDrive">
         <h1>Edit Drive</h1>
         <form onSubmit={this.onSubmit}>
-        <textarea id="date" placeholder="Date" value={newTrip.date} onChange={this.dateChange} /><br/>
-        <textarea id="startTime" placeholder="Start Time" value={newTrip.startTime} onChange={this.startTimeChange} />
-        <textarea id="endTime" placeholder="End Time" value={newTrip.endTime} onChange={this.endTimeChange} /><br/>
-        <input type="submit" value="Save Edit" />
+          <select onChange={this.routeChange}>
+            {this.routesToDropdown()}
+          </select><br/><br/>
+          <textarea id="date" placeholder="Date" value={newTrip.date} onChange={this.dateChange} /><br/>
+          <textarea id="startTime" placeholder="Start Time" value={newTrip.startTime} onChange={this.startTimeChange} />
+          <textarea id="endTime" placeholder="End Time" value={newTrip.endTime} onChange={this.endTimeChange} /><br/>
+          <input type="submit" value="Save Edit" />
       </form>
       </div>
 
--- src/components/FiveDayCard/FiveDayCard.js
+++ src/components/FiveDayCard/FiveDayCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import driveShape from '../../helpers/propz/driveShape';
+// import driveShape from '../../helpers/propz/driveShape';
+import routesData from '../../helpers/data/routesData';
 
 class FiveDayCard extends React.Component {
-  static propTypes = {
-    drive: driveShape.driveCardShape,
+  state = {
+    newRoute: [],
   }
 
   deleteMe = (e) => {
@@ -13,7 +14,18 @@ class FiveDayCard extends React.Component {
     deleteTrip(trip.id, trip.routeId);
   }
 
+  getRoute = () => {
+    routesData.getSingleRoute(this.props.trip.routeId)
+      .then(routePromise => this.setState({ newRoute: routePromise.data }))
+      .catch(err => console.error('could not find route in FiveDayView', err));
+  }
+
+  componentDidMount() {
+    this.getRoute();
+  }
+
   render() {
+    const { newRoute } = this.state;
     const { trip } = this.props;
     const editLink = `/edit/${trip.id}`;
     return (
@@ -21,6 +33,7 @@ class FiveDayCard extends React.Component {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{trip.date}</h5>
+            <p><i>{newRoute.origin} to {newRoute.destination}</i></p>
             <p className="card-text">{trip.startTime} to {trip.endTime}</p>
             <Link className="btn btn-warning" to={editLink}>Edit</Link>
             <button className="btn btn-danger" onClick={this.deleteMe}>x</button>
